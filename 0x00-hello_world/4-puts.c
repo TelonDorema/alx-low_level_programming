dc6770bb170f43aae06869358b0353717d23e41d
TelonDorema <tdorema8gh@gmail.com>
2023-06-19 00:08:03 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - this main is for printing with put
+ * Return: always(0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
