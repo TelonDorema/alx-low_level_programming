7c85b3b4f1aa9c767c7c8ff53a38d12e1414064f
TelonDorema <tdorema8gh@gmail.com>
2023-06-25 17:47:03 -0700

    Printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Printing putchar follwed by a new line
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int j;
+	char c[] = "_putchar";
+
+	for (j = 9; j >= 1; j--)
+	{
+		_putchar(c[j]);
+	}
+	_putchar('\n');
+	return (0);
+}
