4ec8464fe76d6b73d25beff8f041203a08d688c5
TelonDorema <tdorema8gh@gmail.com>
2023-06-26 20:23:27 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string = print string in reverse
+ * @s: reverse output
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int j;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (j = 0; j < counter; j++)
+	{
+		counter--;
+		rev = s[j];
+		s[j] = s[counter];
+		s[counter] = rev;
+	}
+
+}
